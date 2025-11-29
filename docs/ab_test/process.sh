1fc98629f10cc8c9aab809728c696a60ec1f9e8e
Benjamin Berg <ben.berg781@gmail.com>
2025-11-28 21:09:31 -0500

    1764382171: update frame

--- docs/ab_test/process.sh
+++ docs/ab_test/process.sh
@@ -6,3 +6,8 @@ do
     magick -verbose "${base}.jpeg" -resize 600x448 -gravity center -extent 600x448 "${base}_small.jpeg"
     echo $base
 done
+
+ts=$(date +%s) 
+echo $ts > manifest.txt
+ls ./*_small.jpeg | wc -l >> manifest.txt
+git commit -a -m "$ts: update frame"
